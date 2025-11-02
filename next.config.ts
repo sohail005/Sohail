979c9adf81a3a276c0be0213cc70314d3bd05053
sohail005 <sohail345patel@gmail.com>
2025-11-02 18:49:31 +0530

    Refactor configuration for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Enable static export
-  output: "export",
-
-  // Replace `my-next-app` with your actual repo name
-  basePath: "/sohail",
-  assetPrefix: "/sohail/",
-
-  // Disable next/image optimization since GH Pages is static
-  images: {
-    unoptimized: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
