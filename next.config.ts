7a913be8a0fbb8ff99061f9367bde9e27d3b3e09
sohail005 <sohail345patel@gmail.com>
2025-11-02 18:40:26 +0530

    Setup GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Enable static export
+  output: "export",
+
+  // Replace `my-next-app` with your actual repo name
+  basePath: "/sohail",
+  assetPrefix: "/sohail/",
+
+  // Disable next/image optimization since GH Pages is static
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
