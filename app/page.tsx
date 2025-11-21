740db5ad63c3bb17aa6a5632b49ae066003cf16b
sohail005 <sohail345patel@gmail.com>
2025-11-22 03:41:53 +0530

    Update layout and page components; add Leaflet map integration and global types

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+"use client";
 
-export default function page() {
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const locations = [
+  { name: "New York", lat: 40.7128, lng: -74.006 },
+  { name: "London", lat: 51.5074, lng: -0.1278 },
+  { name: "Tokyo", lat: 35.6895, lng: 139.6917 },
+  { name: "Paris", lat: 48.8566, lng: 2.3522 },
+  { name: "Sydney", lat: -33.8688, lng: 151.2093 },
+  { name: "Toronto", lat: 43.65107, lng: -79.347015 },
+  { name: "Dubai", lat: 25.2048, lng: 55.2708 },
+  { name: "Singapore", lat: 1.3521, lng: 103.8198 },
+  { name: "Berlin", lat: 52.52, lng: 13.405 },
+  { name: "Cape Town", lat: -33.9249, lng: 18.4241 },
+];
+
+const icon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+export default function Page() {
   return (
-    <div>
-      <h1>Hello, Sohail!</h1>
-    </div>
-  )
+    <MapContainer
+      center={[20, 0]}
+      zoom={2}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; OpenStreetMap contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {locations.map((loc, i) => (
+        <Marker key={i} position={[loc.lat, loc.lng]} icon={icon}>
+          <Tooltip>{loc.name}</Tooltip>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
 }
